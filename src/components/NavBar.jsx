698adfe718d0e65f731bbb6bc4e08b8298259313
minli999 <presley.us@gmail.com>
2022-06-21 12:36:03 -0400

    Adding alternative nav bar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,7 +1,9 @@
-import React from 'react';
+import React, { useState } from 'react';
 import {FaBars, FaTimes} from 'react-icons/fa';
 
 const NavBar = () => {
+
+  const [nav, setNav] = useState(false);
   const links = [
     {
       id: 1,
@@ -32,7 +34,7 @@ const NavBar = () => {
         <h1 className="text-5xl font-signature ml-2">Elvis Li</h1>
       </div>
 
-      <ul className="flex">
+      <ul className="hidden md:flex">
         {links.map(({id,link}) => (
           <li key={links.id} 
           className="px-4 cursor-pointer capitalize font-medium text-gray-500 hover:scale-105 duration-200"
@@ -41,6 +43,20 @@ const NavBar = () => {
           </li>
         ))}
 
+      </ul>
+
+      <div onClick={() => setNav(!nav)} className="cursor-pointer pr-4 z-10 text-gray-500">
+        {nav ? <FaTimes size={30} /> : <FaBars size={30} />}
+      </div>
+
+      <ul className="flex flex-col justify-center items-center absolute top-0 w-full h-screen bg-gradient-to-b from -black to-gray-800 text-gray-500">
+          {links.map(({id,link}) => (
+          <li key={links.id} 
+          className="px-4 cursor-pointer capitalize py-6 text-4xl"
+          >
+            {link}
+          </li>
+        ))}
       </ul>
     </div>
   );

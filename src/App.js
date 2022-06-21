eea311b4e03703ebace5e1232cb1d7d02764e4d9
minli999 <presley.us@gmail.com>
2022-06-21 12:00:29 -0400

    Modifying NavBar with multiple options

--- src/App.js
+++ src/App.js
@@ -1,10 +1,9 @@
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
     <div>
-      <h1 className="text-5xl font-bold 
-      font-signature">Hello</h1>
-      
+      <NavBar />
     </div>
   );
 }
